226432cac5e5b15d1078e1c373c23a4fc33897ba
RuslanHizhytskyi <r.hizhytskyi@gmail.com>
2020-11-14 16:56:52 +0200

    apdate slider/ add dots

--- js/main.js
+++ js/main.js
@@ -330,16 +330,100 @@ document.addEventListener('DOMContentLoaded', () => {
   // Teacher slider
   
   const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         next = document.querySelector('.offer__slider-next'),
         prev = document.querySelector('.offer__slider-prev'),
         current = document.getElementById('current'),
-        total = document.getElementById('total');
+        total = document.getElementById('total'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width;
 
-  let sliderIndex = 1;
+  let sliderIndex = 1,
+      offset = 0;
 
+  total.textContent = getZero(slides.length);
+  current.textContent = getZero(sliderIndex);
+
+  slidesField.style.width = 100 * slides.length + '%';
+  slidesField.style.display = 'flex';
+  slidesField.style.transition = '0.5s all';
+
+  slidesWrapper.style.overflow = 'hidden';
+
+  slides.forEach(slide => {
+    slide.style.width = width;
+  });
+
+  const indicators = document.createElement('ol'),
+        dots = [];
+  indicators.classList.add('carousel-indicators');
+  slider.append(indicators);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.classList.add('dot');
+    indicators.append(dot);
+    dots.push(dot);
+    if (i == 0) {
+      dot.classList.add('active');
+    }
+  }
+
+  next.addEventListener('click', () => {
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (sliderIndex == slides.length) {
+      sliderIndex = 1;
+    } else {
+      sliderIndex++;
+    }
+    current.textContent = getZero(sliderIndex);
+    dots.forEach (dot => dot.classList.remove('active'));
+    dots[sliderIndex - 1].classList.add('active');
+  });
+
+  prev.addEventListener('click', () => {
+    if (offset == 0) {
+      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+    } else {
+      offset -= +width.slice(0, width.length - 2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+    
+    if (sliderIndex == 1) {
+      sliderIndex = slides.length;
+    } else {
+      sliderIndex--;
+    }
+    current.textContent = getZero(sliderIndex);
+    dots.forEach (dot => dot.classList.remove('active'));
+    dots[sliderIndex - 1].classList.add('active');
+  });
+
+  dots.forEach(dot => {
+    dot.addEventListener('click', (evt) => {
+      const slideTo = evt.target.getAttribute('data-slide-to');
+
+      sliderIndex = slideTo;
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+      current.textContent = getZero(sliderIndex);
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      dots.forEach (dot => dot.classList.remove('active'));
+      dots[sliderIndex - 1].classList.add('active');
+    });
+  });
   // showSlides(sliderIndex);
 
-  // total.textContent = getZero(slides.length);
 
   // function showSlides(n) {
   //   if (n > slides.length) {
